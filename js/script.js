e8ed42284954b5d982d71a59ac6cb3f76d6289c6
Oleksandr Karas <al.karas.pr@gmail.com>
2022-11-30 12:21:22 +0200

    change js script to copy to clip board (30%)

---
+++ js/script.js
@@ -0,0 +1,21 @@
+// // let copyBtn1 = document.getElementsByClassName("button-copy")[0];
+// // let copyBtn2 = document.getElementsByClassName("button-copy")[1];
+// // copyBtn.addEventListener('click', copyToClipBoard());
+// let copyButtons = document.querySelectorAll(".button-copy");
+// let copyTexts = document.querySelectorAll('#copied-text');
+
+// copyButtons[0].addEventListener('click', copyToClipBoard(0));
+
+// // copyButtons[1].addEventListener('click', copyToClipBoard(1));
+// // copyButtons[2].addEventListener('click', copyToClipBoard(2));
+// copyButtons[3].addEventListener('click', copyToClipBoard(3));
+// // copyButtons[4].addEventListener('click', copyToClipBoard(4));
+
+// function copyToClipBoard(index) {
+//     let copyText = copyTexts[index].innerHTML;
+//     navigator.clipboard.writeText(copyText).then(() => {
+//         console.log("text");
+//     })
+// }
+
+
